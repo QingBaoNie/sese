523605563806efc15338e4524b1291e0e5a570dd
QingBao <148045149+QingBaoNie@users.noreply.github.com>
2025-08-08 15:55:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,29 @@ from astrbot.api import logger
 import random
 from .back import time_long, volume
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register(
+    name="撸一下",  # 插件唯一识别名
+    author="Qing",  # 作者
+    description="这是 AstrBot 的插件",  # 插件简短描述
+    version="v1.0",  # 插件版本
+    help="撸一下",  # 插件帮助信息
+    repo="https://github.com/QingBaoNie/sese"  # 插件仓库地址
+)
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
     
     @filter.message()
     async def dajiao(self, event: AstrMessageEvent):
-        # 获取消息内容
         msg = event.message_str.strip()
         
-        # 判断是否包含触发词
         if msg == "撸一下":
-            time = round(random.uniform(1, 600), 2)
+            time_val = round(random.uniform(1, 600), 2)
             V = round(random.uniform(0.01, 100), 2)
-            a = time_long(time)
+            a = time_long(time_val)
             b = volume(V)
             user_name = event.get_sender_name()
             
             yield event.plain_result(
-                f"{user_name}，你撸了{time}s，{a} 射出了{V}ml，{b}!"
+                f"{user_name}，你撸了 {time_val}s，{a} 射出了 {V}ml，{b}!"
             )
