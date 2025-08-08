4bfa427c42a5ed633509c8a959e749a84b727050
QingBao <148045149+QingBaoNie@users.noreply.github.com>
2025-08-08 16:03:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from astrbot.api.event import filter
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-from astrbot.api.event import AstrMessageEvent
 import random
 from .back import time_long, volume
 
