58a203f53afddc9d485201cb4e49921b069a7b8e
QingBao <148045149+QingBaoNie@users.noreply.github.com>
2025-08-08 15:53:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import random
+from .back import time_long, volume
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+    @filter.message()
+    async def dajiao(self, event: AstrMessageEvent):
+        # 获取消息内容
+        msg = event.message_str.strip()
+        
+        # 判断是否包含触发词
+        if msg == "撸一下":
+            time = round(random.uniform(1, 600), 2)
+            V = round(random.uniform(0.01, 100), 2)
+            a = time_long(time)
+            b = volume(V)
+            user_name = event.get_sender_name()
+            
+            yield event.plain_result(
+                f"{user_name}，你撸了{time}s，{a} 射出了{V}ml，{b}!"
+            )
